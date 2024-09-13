9cafb4c9623b8d22b4b471fe64f1d16de596a9d0
John Leen <jleen@saturnvalley.org>
2024-09-13 11:57:17 -0400

    Delimit

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-import { writeFile } from 'node:fs/promises'
-import { Readable } from 'node:stream'
-import path from 'node:path'
+import { writeFile } from 'node:fs/promises';
+import { Readable } from 'node:stream';
+import path from 'node:path';
 
-import { JSDOM } from 'jsdom'
-import { Readability } from '@mozilla/readability'
-import TurndownService from 'turndown'
+import { JSDOM } from 'jsdom';
+import { Readability } from '@mozilla/readability';
+import TurndownService from 'turndown';
 
-let url = process.argv[2]
-let resp = await fetch(url)
-let html = await resp.text()
-let dom = new JSDOM(html, { url: url })
-let reader = new Readability(dom.window.document)
-let article = reader.parse()
+let url = process.argv[2];
+let resp = await fetch(url);
+let html = await resp.text();
+let dom = new JSDOM(html, { url: url });
+let reader = new Readability(dom.window.document);
+let article = reader.parse();
 
-let readableDom = new JSDOM(article.content, { url: url })
+let readableDom = new JSDOM(article.content, { url: url });
 for (let img of readableDom.window.document.getElementsByTagName('img')) {
-    let offlineImg = 'img' + Math.random() + path.extname(img.src)
-    let imgResp = await fetch(img.src)
-    let body = Readable.fromWeb(imgResp.body)
-    await writeFile(`out/${offlineImg}`, body)
-    console.log(`${img.src} -> ${offlineImg}`)
-    img.src = offlineImg
+    let offlineImg = 'img' + Math.random() + path.extname(img.src);
+    let imgResp = await fetch(img.src);
+    let body = Readable.fromWeb(imgResp.body);
+    await writeFile(`out/${offlineImg}`, body);
+    console.log(`${img.src} -> ${offlineImg}`);
+    img.src = offlineImg;
 }
 
 let turndown = new TurndownService({
@@ -29,7 +29,7 @@ let turndown = new TurndownService({
     bulletListMarker: '-',
     codeBlockStyle: 'fenced',
     emDelimiter: '*'
-})
-let md = turndown.turndown(readableDom.window.document.documentElement.outerHTML)
+});
+let md = turndown.turndown(readableDom.window.document.documentElement.outerHTML);
 
-await writeFile('out/article.md', md)
+await writeFile('out/article.md', md);
