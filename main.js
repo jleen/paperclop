3b8fc119976dfc59afa992b65604648de334cd9d
John Leen <jleen@saturnvalley.org>
2024-09-13 00:21:09 -0400

    Do the thing

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import { writeFile } from 'node:fs/promises'
+import { Readable } from 'node:stream'
+import path from 'node:path'
+
 import { JSDOM } from 'jsdom'
 import { Readability } from '@mozilla/readability'
 import TurndownService from 'turndown'
@@ -5,14 +9,16 @@ import TurndownService from 'turndown'
 let url = process.argv[2]
 let resp = await fetch(url)
 let html = await resp.text()
-let dom = new JSDOM(html,
-                    { url: url })
+let dom = new JSDOM(html, { url: url })
 let reader = new Readability(dom.window.document)
 let article = reader.parse()
 
 let readableDom = new JSDOM(article.content, { url: url })
 for (let img of readableDom.window.document.getElementsByTagName('img')) {
-    let offlineImg = 'img' + Math.random()
+    let offlineImg = 'img' + Math.random() + path.extname(img.src)
+    let imgResp = await fetch(img.src)
+    let body = Readable.fromWeb(imgResp.body)
+    await writeFile(`out/${offlineImg}`, body)
     console.log(`${img.src} -> ${offlineImg}`)
     img.src = offlineImg
 }
@@ -26,4 +32,4 @@ let turndown = new TurndownService({
 })
 let md = turndown.turndown(readableDom.window.document.documentElement.outerHTML)
 
-console.log(md)
+await writeFile('out/article.md', md)
