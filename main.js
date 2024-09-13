4fde222b739d37d4a945745b51b19465a7b37ad6
John Leen <jleen@saturnvalley.org>
2024-09-12 23:19:54 -0400

    And here we are

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ import { JSDOM } from 'jsdom'
 import { Readability } from '@mozilla/readability'
 import TurndownService from 'turndown'
 
-let html = '<h1>Greetings, creatures of wherewhen</h1>Greetings from <font size="50">beyondbetween</font>'
+let url = process.argv[2]
+let resp = await fetch(url)
+let html = await resp.text()
 let doc = new JSDOM(html,
-                    { url: 'https://nyan.cat'})
+                    { url: url })
 let reader = new Readability(doc.window.document)
 let article = reader.parse()
-let turndown = new TurndownService({ headingStyle: 'atx' })
+let turndown = new TurndownService({
+    headingStyle: 'atx',
+    hr: '---',
+    bulletListMarker: '-',
+    codeBlockStyle: 'fenced',
+    emDelimiter: '*'
+})
 let md = turndown.turndown(article.content)
+
 console.log(md)
