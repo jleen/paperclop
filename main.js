8618095df1262daa6f690e616a99483a7315f513
John Leen <jleen@saturnvalley.org>
2024-09-13 13:49:29 -0400

    Cleanup

--- main.js
+++ main.js
@@ -1,35 +1,30 @@
 import { writeFile } from 'node:fs/promises';
 import { Readable } from 'node:stream';
-import path from 'node:path';
+import { extname } from 'node:path';
 
 import { JSDOM } from 'jsdom';
 import { Readability } from '@mozilla/readability';
 import TurndownService from 'turndown';
 
 let url = process.argv[2];
-let resp = await fetch(url);
-let html = await resp.text();
+let response = await fetch(url);
+let html = await response.text();
 let dom = new JSDOM(html, { url: url });
-let reader = new Readability(dom.window.document);
-let article = reader.parse();
+let article = new Readability(dom.window.document).parse();
 
-let readableDom = new JSDOM(article.content, { url: url });
-for (let img of readableDom.window.document.getElementsByTagName('img')) {
-    let offlineImg = 'img' + Math.random() + path.extname(img.src);
-    let imgResp = await fetch(img.src);
-    let body = Readable.fromWeb(imgResp.body);
+let doc = new JSDOM(article.content, { url: url }).window.document;
+for (let img of doc.getElementsByTagName('img')) {
+    let offlineImg = 'img' + Math.random() + extname(img.src);
+    let imgresponse = await fetch(img.src);
+    let body = Readable.fromWeb(imgresponse.body);
     await writeFile(`out/${offlineImg}`, body);
     console.log(`${img.src} -> ${offlineImg}`);
     img.src = offlineImg;
 }
 
 let turndown = new TurndownService({
-    headingStyle: 'atx',
-    hr: '---',
-    bulletListMarker: '-',
-    codeBlockStyle: 'fenced',
-    emDelimiter: '*'
-});
-let md = turndown.turndown(readableDom.window.document.documentElement.outerHTML);
+    headingStyle: 'atx', hr: '---', bulletListMarker: '-',
+    codeBlockStyle: 'fenced', emDelimiter: '*' });
+let md = turndown.turndown(doc.documentElement.outerHTML);
 
 await writeFile('out/article.md', md);
